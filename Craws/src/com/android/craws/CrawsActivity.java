247925686db9dc34e8ed6d133184b98c1b36a8c9
arunimmanuelpro <arunimmanuelpro@gmail.com>
2014-02-02 02:44:36 +0530

    Added Sysout

--- Craws/src/com/android/craws/CrawsActivity.java
+++ Craws/src/com/android/craws/CrawsActivity.java
@@ -9,5 +9,6 @@ public class CrawsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        System.out.println("Welcome to Craws");
     }
 }
